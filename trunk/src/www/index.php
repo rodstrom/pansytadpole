ac3482d3bff9b0d1c24d31de696b67016ad70e32
herman.rodstrom <herman.rodstrom@07666076-cecb-11de-b920-cb03934a6abc>
2010-01-04 18:57:03 +0000

    optimized some code in gamearea, small changes

--- trunk/src/www/index.php
+++ trunk/src/www/index.php
@@ -3,7 +3,7 @@
 <title>Pansy Tadpole MMOTag</title>
 </head>
 <body>
-<applet archive="PansyTadpole.jar" code=client.PansyTadpole.class width=100% height=100% >
+<applet archive="PansyTadpole.jar" code=client.PansyTadpole.class width=1280 height=800 >
 	<param name="host" value="server.rodstrom.se">
 	<param name="nick" value="web"> 
 </applet> 
